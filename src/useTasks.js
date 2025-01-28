eeb8eb441da8437f9790a7fe1ec646e746359dc0
Mateusz Pietroń <mpietron357@gmail.com>
2025-01-28 18:09:07 +0100

    add components form app to useTasks

---
+++ src/useTasks.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { defaultTasks } from "./defaultTasks";
+
+export const useTasks = () => {
+    const getInitialTask = () => {
+        const localStorageTask = localStorage.getItem(tasks);
+        if (localStorageTask === null) {
+            return defaultTasks;
+        }
+        return JSON.parse(localStorage.getItem(tasks));
+    }
+    const [tasks, setTasks] = useState(getInitialTask);
+
+    useEffect(() => {
+        localStorage.setItem(tasks, JSON.stringify(tasks));
+    }, [tasks]);
+
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    };
+    useEffect(() => {
+
+    })
+    const toggleTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task =>
+            task.id === id ? { ...task, done: !task.done } :
+                task
+        ));
+    }
+    const setAllDone = () => {
+        setTasks(tasks => tasks.map(task => ({
+            ...task,
+            done: true,
+        })));
+    };
+    const addNewTask = (content) => {
+        setTasks(task => [
+            ...task,
+            {
+                content,
+                done: false,
+                id: task.length ? task[tasks.length - 1].id + 1 : 1,
+            }
+        ]);
+        return {
+            tasks,
+            removeTask,
+            toggleTaskDone,
+            setAllDone,
+            addNewTask,
+        };
+    };
+}
