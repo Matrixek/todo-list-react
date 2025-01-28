7509b20443aad076d7d5d8f147e2e9938996b490
Mateusz Pietroń <mpietron357@gmail.com>
2025-01-28 18:08:28 +0100

    add import in app and remove components to another folder

--- src/App.js
+++ src/App.js
@@ -1,70 +1,42 @@
-import React, {useEffect, useState,} from 'react';
+import React, { useState } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import {useTasks} from "./useTasks";
 
-const defaultTasks = [
-  { id: 1, content: "przejść na Reacta", done: false },
-  { id: 2, content: "zjeść kolację", done: true },
-];
+
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || defaultTasks);
-  
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
 
   };
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-  useEffect(() => {
+  const {
+    tasks,
+    removeTasks,
+    toggleTasksDone,
+    setAllDone,
+    addNewTasks,
+  } = useTasks();
 
-  })
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task =>
-      task.id === id ? { ...task, done: !task.done } :
-        task
-    ));
-  }
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task,
-      done: true,
-    })));
-  };
-  const addNewTask = (content) => {
-    setTasks(task => [
-      ...task,
-      {
-        content,
-        done: false,
-        id: task.length ? task[tasks.length - 1].id + 1 : 1,
-      }
-    ]);
-  };
-  
   return (
     <Container >
       <Header title="Lista zadań" />
       <Section title=
         "Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form addNewTasks={addNewTasks} />}
       />
       <Section
         title="Lista zadań "
         body={
           <Tasks tasks={tasks}
             hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
+            removeTasks={removeTasks}
+            toggleTasksDone={toggleTasksDone}
           />
         }
         extraHeaderContent={
