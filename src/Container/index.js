20951e56f2b06c231507cc760b49c61484697e59
Mateusz Pietroń <mpietron357@gmail.com>
2025-02-08 22:24:17 +0100

    change className to  Styled in Container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./style.css";
+import { StyledContainer } from "./styled";
 
-const Container = ({children}) => (
-    <main className="container">
+const Container = ({ children }) => (
+    <StyledContainer>
         {children}
-    </main>
+    </StyledContainer>
 );
 export default Container;
