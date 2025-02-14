aa1505e072e31faeeb79467a9d4f334fd5942687
Mateusz Pietroń <mpietron357@gmail.com>
2025-02-14 21:53:19 +0100

    create theme

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    colors: {
+      white: "#fff",
+      grey: "#eee",
+      teal: "#008080",
+      hoverBtn: "#20b2aa",
+      disabledBtn: "#bbb",
+      formBtnHover: "#20b2aa",
+      taskBtn: "#008000",
+      taskBtnHover: "#00b300",
+      taskBtnRemove: "#cc0000",
+      taskBtnRemoveHover: "#ff3330",
+        green: "#67A712",
+        red:"#C7031E",
+    },
+    breakpoint: {
+      mobile: 767,
+    },
+  };
